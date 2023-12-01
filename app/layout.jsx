84b62641c82e378c93824b9583c99e749f9f42ed
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-01 20:28:18 +0600

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,37 +8,37 @@ import "./nprogress.css";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
-	title: "Alvalens | Portofolio",
+	title: "Alamin | Portofolio",
 	description:
-		"My name is Alvalens, I'm a web developer and I'm passionate about it. I'm currently studying at Universitas Negeri Malang.",
-	author: "Alvalen Shafelbilyunazra",
-	siteUrl: "https://www.alvalens.my.id",
-	applicationName: "Alvalens",
+		"My name is Al-amin, I'm a web developer and I'm passionate about it. I'm currently studying at Universitas Negeri Malang.",
+	author: "Mohammad Al-amin",
+	siteUrl: "https://netquize.vercel.app",
+	applicationName: "Al-amins",
 	keywords: [
-		"alvalens",
-		"alvalen",
-		"alvalen shafel",
-		"shafel",
-		"alvalen shafelbilyunazra",
-		"alvalen shafel bilyunazra",
-		"bloodfallen",
-		"alvalen porto",
-		"alvalen um",
+		"alamin",
+		"alamin2k23",
+		"mohammad alamin",
+		"al-amin",
+		"mohammad al-amin",
+		"netquize",
+		"alamin portfolio",
+		"alamin porto",
+		"alvalin",
 	],
 	openGraph: {
 		type: "website",
-		url: "https://www.alvalens.my.id",
-		title: "Alvalens | Portofolio",
-		description: "My name is Alvalens, This is my portofolio website.",
+		url: "https://netquize.vercel.app",
+		title: "Al-amin | Portofolio",
+		description: "My name is Al-amin, This is my portofolio website.",
 		width: 1200,
 		height: 630,
 		images: [
 			{
 				url: "/og-image-rev.png",
-				alt: "Alvalens Portofolio",
+				alt: "Al-amin Portofolio",
 			},
 		],
-		site_name: "Alvalens | Portofolio",
+		site_name: "Al-amins | Portofolio",
 	},
 };
 
