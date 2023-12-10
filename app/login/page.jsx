d75ea995af4e4eaa8ba1cb619c398ca38129b334
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-10 17:49:08 +0600

    Create page.jsx

---
+++ app/login/page.jsx
@@ -0,0 +1,149 @@
+"use client";
+import { useEffect } from "react";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import Button from "@/components/Button";
+import Image from "next/legacy/image";
+import FixedButon from "@/components/FixedButton";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+
+
+// images
+import Me1 from "@/public/img/me1.jpg";
+import Me2 from "@/public/img/me2.jpg";
+import Me3 from "@/public/img/me3.jpg";
+import Hr from "@/components/Hr";
+export default function Page() {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	
+	const handleBack = () => {
+		window.location.href = "/";
+	};
+	return (
+		<>
+			<main className="overflow-hidden">
+				<FixedButon onClick={handleBack}>
+					<FontAwesomeIcon
+						icon={faChevronLeft}
+						className="text-black pr-10"
+					/>
+				</FixedButon>
+				<div className="relative h-screen w-screen  gap-4 p-10 flex justify-center items-center flex-col mb-10 overflow-hidden">
+					<div className="z-0 mb-48 md:mb-0  md:absolute top-1/4  md:right-[10%] md:-translate-y-16 ">
+						<motion.div
+							initial={{ scale: 1 }}
+							animate={{ scale: 1.6 }}
+							transition={{ ease: "circOut", duration: 1 }}
+							className="bg-slate-300 rounded-sm h-[400px] md:h-[600px] w-[80vw] md:w-[30vw] grayscale hover:grayscale-0">
+							  <div className="flex justify-center mt-8">
+      <div style={{ minWidth: "30%" }}>
+        <div className="flex min-h-full shadow-lg flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-white">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <div className="flex justify-center">
+              <Image src="/login.gif" height={100} width={100} />
+            </div>
+            <h2 className="mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Create a new account
+            </h2>
+          </div>
+
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="confirmpassword"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Confirm Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="confirmpassword"
+                    name="confirmpassword"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Already have an account?{" "}
+              <span
+                onClick={() => {
+                  router.push("/");
+                }}
+                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 cursor-pointer"
+              >
+                Sign In
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+							
+					</motion.div>
+				</div>
+			</main>
+		</>
+	);
+}
