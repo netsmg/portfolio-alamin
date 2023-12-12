1901891a56497830aa4dd0bf7a4f34464aed2e53
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-12 15:12:03 +0600

    Create mcq.jsx

---
+++ app/mcq/mcq.jsx
@@ -0,0 +1,37 @@
+// pages/mcq.js
+import { useState } from 'react';
+import QuestionComponent from '../components/QuestionComponent';
+
+const MCQPage = () => {
+  const [userResponses, setUserResponses] = useState([]);
+
+  const questions = [
+    {
+      question: 'What is the capital of France?',
+      options: ['Berlin', 'Madrid', 'Paris', 'Rome'],
+      correctAnswer: 'Paris',
+    },
+    // Add more questions as needed
+  ];
+
+  const handleQuestionSubmit = (userResponse) => {
+    setUserResponses([...userResponses, userResponse]);
+    // You can store or process the responses as needed
+  };
+
+  return (
+    <div>
+      {questions.map((question, index) => (
+        <QuestionComponent
+          key={index}
+          question={question.question}
+          options={question.options}
+          onSubmit={(userResponse) => handleQuestionSubmit({ questionIndex: index, userResponse })}
+        />
+      ))}
+      {/* Display results or perform additional actions */}
+    </div>
+  );
+};
+
+export default MCQPage;
