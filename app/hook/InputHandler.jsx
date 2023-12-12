cb5eec1e33bffffe9041cebbb8753dfa0e517b40
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-12 16:29:27 +0600

    Create InputHandler.jsx

---
+++ app/hook/InputHandler.jsx
@@ -0,0 +1,24 @@
+"use client";
+import { useState } from "react";
+
+
+
+export  default function    InputHandler(data,additionalFunc=null,setIn=false){
+    const   [input,setInput]=useState(data)
+    const   onChange=(e)=>{
+        setInput(e.target.value)
+        additionalFunc&&additionalFunc(e.target.value)
+    }
+
+//  let data={
+//         value:input,
+//         onChange:onChange,
+//         setInput:setInput
+//     }
+    return{
+        value:input,
+        onChange:onChange,
+        setInput:setInput
+    
+    }
+}
