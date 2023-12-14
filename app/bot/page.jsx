ed0368e80e5147879f6a408c9ac3e7b3baff965e
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-14 19:07:26 +0600

    Update page.jsx

--- app/bot/page.jsx
+++ app/bot/page.jsx
@@ -61,6 +61,7 @@ const Chatbot = () => {
   };
 
   return (
+    <>
     <div className="main-container">
       <div id="chatbot-container">
         {/* Render chat history */}
@@ -86,6 +87,7 @@ const Chatbot = () => {
       </button>
       <style dangerouslySetInnerHTML={{__html: "*{\n\n margin:0;\n\n padding:0;\n\n box-sizing:border-box;\n\n}\n\nbody{\n\n width:100%;\n\n min-height:100vh;\n\n}\n\n.main-container{\n\n margin:0 auto;\n\n width:100%;\n\n height:100%;\n\n max-width:500px;\n\n padding:20px 0;\n\n}\n\n#chatbot-container{\n\n width:95%;\n\n height:350px;\n\n margin:10px auto;\n\n padding:10px 5px;\n\n overflow-y:auto;\n\n background:#f3f3f3;\n\n display:flex;\n\n display:-ms-flexbox;\n\n display:inline-flex;\n\n display:-webkit-flex;\n\n flex-direction: column;\n\n border-radius:10px;\n\n}\n\n.speech::first-letter,\n\n.recorder::first-letter{\n\n text-transform: capitalize;\n\n}\n\n#chatbot-container .recorder{\n\n color:#fff;\n\n background:#78f;\n\n border-radius:5px;\n\n padding:8px;\n\n margin:4px 0;\n\n max-width:250px;\n\n align-self:flex-start;\n\n flex-shrink: 0;\n\n \n\n}\n\n#chatbot-container .speech{\n\n color:#fff;\n\n background:#1c8;\n\n border-radius:5px;\n\n padding:8px;\n\n margin:4px 0;\n\nflex-shrink: 0;\n\n align-self:flex-end;\n\n max-width:250px;\n\n}\n\nbutton{\n\n border:none;\n\n outline:none;\n\n background:none;\n\n}\n\n.fa-telegram-plane{\n\n font-size:36px;\n\n width:15%;\n\n padding:8px 12px;\n\n line-height: 30px;\n\n cursor: pointer;\n\n display:inline-block;\n\n}\n\n.fa-telegram-plane:hover{\n\n color:#78f;\n\n}\n\n#btn{\n\n background:#78f;\n\n color:whiteSmoke;\n\n padding:10px 25px;\n\n font-size:22px;\n\n font-weight:600;\n\n display:block;\n\n box-shadow:0 3px 10px #a7f;\n\n margin:0 auto ;\n\n transition:0.5s;\n\n}\n\n#btn:hover{\n\n padding-right: 30px;\n\n background:#6a7;\n\n transition:0.5s;\n\n}\n\n#form{\n\n width:100%;\n\n text-align: center;\n\n margin-bottom: 8px;\n\n}\n\n#form #botvalue{\n\n padding:8px 10px;\n\n border-radius:6px;\n\n outline:none;\n\n width:calc(100% - 18%);\n\n line-height: 30px;\n\n border:1px solid #555;\n\n font-size:17px;\n\n font-weight:normal;\n\n}" }} />
     </div>
+    </>
   );
 };
 
