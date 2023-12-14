61899edc074eb64c00f1264c27d849deb62445be
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-14 18:10:07 +0600

    Update layout.jsx

--- app/demo/quize/layout.jsx
+++ app/demo/quize/layout.jsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/Footer";
 
 export const metadata = {
-	title: "Demo | quize",
+	title: "Demo | Quize",
 };
 export default function Layout({ children }) {
   return (
