e2735fb65ef36f8759d44bb37bfaf93915123c2e
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-14 18:08:02 +0600

    Update page.jsx

--- app/demo/quize/page.jsx
+++ app/demo/quize/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useQuizContext } from "@/components/QuizContext";
-import { useNavigate, useParams } from "react-router-dom";
+
 
 function Page() {
   const { level } = useParams();
